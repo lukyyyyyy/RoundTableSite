55bd9c595184c543a03399d753a1b25a9fb6ed29
lukyyyyyy <98971269+lukyyyyyy@users.noreply.github.com>
2024-04-01 22:09:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Your logic for form submission goes here
+    alert('Form submitted!');
+  });
+  
